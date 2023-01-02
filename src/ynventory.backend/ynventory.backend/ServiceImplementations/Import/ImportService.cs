3a1c6b5743d525df636b80bdf6ce4aacba29247f
pluethi1 <philipp_luethi@swissonline.ch>
2023-01-02 20:55:42 +0100

    - fixed formatting - changed import logic

--- src/ynventory.backend/ynventory.backend/ServiceImplementations/Import/ImportService.cs
+++ src/ynventory.backend/ynventory.backend/ServiceImplementations/Import/ImportService.cs
@@ -1,28 +1,27 @@
-using CsvHelper;
-using System.Globalization;
-using Ynventory.Backend.Services.Import;
+using Microsoft.EntityFrameworkCore;
+using System.Text;
 using Ynventory.Backend.Contracts.Requests;
 using Ynventory.Backend.Contracts.Responses;
-using Ynventory.Data;
-using Microsoft.EntityFrameworkCore;
-using Ynventory.Data.Models;
-using Ynventory.Backend.Services.Data;
-using Ynventory.Data.Enums;
 using Ynventory.Backend.Exceptions;
+using Ynventory.Backend.Services.Import;
+using Ynventory.Data;
+using Ynventory.Data.Enums;
+using Ynventory.Data.Models;
 
 namespace Ynventory.Backend.ServiceImplementations.Import
 {
     public class ImportService : IImportService
     {
         private readonly YnventoryDbContext _context;
-        private readonly ICollectionService _collectionService;
+        private readonly IServiceProvider _serviceProvider;
 
-        public ImportService(YnventoryDbContext context, ICollectionService collectionService) {
+        public ImportService(YnventoryDbContext context, IServiceProvider serviceProvider)
+        {
             _context = context;
-            _collectionService = collectionService;
+            _serviceProvider = serviceProvider;
         }
 
-        public async Task<IEnumerable<ImportTaskResponse>> getTasks()
+        public async Task<IEnumerable<ImportTaskResponse>> GetTasks()
         {
             var tasks = await _context.ImportTasks.ToArrayAsync();
             return tasks.Select(ToResponse).ToArray();
@@ -42,13 +41,12 @@ namespace Ynventory.Backend.ServiceImplementations.Import
                 throw new CollectionItemNotFoundException(request.CollectionItemId);
             }
 
-
             var task = new ImportTask
             {
                 FileName = request.FileName,
-                FileData = System.Text.Encoding.Default.GetBytes(request.FileData),
+                FileData = Encoding.Default.GetBytes(request.FileData),
                 TaskType = request.TaskType,
-                TaskState = Ynventory.Data.Enums.ImportTaskState.Pending,
+                TaskState = ImportTaskState.Pending,
                 createdAt = DateTime.UtcNow
             };
 
@@ -56,19 +54,16 @@ namespace Ynventory.Backend.ServiceImplementations.Import
 
             await _context.SaveChangesAsync();
 
-            switch(task.TaskType){
-                case ImportTaskType.DelverCSV:
-                    Import_DelverCSV(task);
-                    break;
-            }
+            var import = task.TaskType switch
+            {
+                ImportTaskType.DelverCSV => new DelverCSVImport(_serviceProvider),
+                _ => throw new InvalidDataException(),
+            };
+
+            await import.Run(task);
 
             return ToResponse(task);
         }
-
-
-
-
-
 
         private static ImportTaskResponse ToResponse(ImportTask task)
         {
@@ -77,12 +72,11 @@ namespace Ynventory.Backend.ServiceImplementations.Import
                 Id = task.Id,
                 FileName = task.FileName,
                 TaskType = task.TaskType,
-                Errors = (task.ImportErrors == null) ? new ImportErrorResponse[0] : task.ImportErrors.Select(ToResponse).ToArray(),
+                Errors = (task.ImportErrors == null) ? Array.Empty<ImportErrorResponse>() : task.ImportErrors.Select(ToResponse).ToArray(),
                 createdAt = task.createdAt,
                 finishedAt = task.finishedAt
             };
         }
-
 
         private static ImportErrorResponse ToResponse(ImportError taskError)
         {
@@ -91,48 +85,6 @@ namespace Ynventory.Backend.ServiceImplementations.Import
                 Id = taskError.Id,
                 Error = taskError.Error
             };
-        }
-
-
-
-        private async Task Import_DelverCSV(ImportTask task){
-            using var reader = new StringReader(System.Text.Encoding.Default.GetString(task.FileData));
-            using var csvReader = new CsvReader(reader, CultureInfo.InvariantCulture);
-
-            var result = true;
-            var records = new List<Card>();
-            csvReader.Read();
-            csvReader.ReadHeader();
-            while (csvReader.Read())
-            {
-                try {
-                    await _collectionService.CreateCard(task.CollectionItem.CollectionId, task.CollectionItemId, new CardCreateRequest(){
-                        CardMetadataId = csvReader.GetField<Guid>("Scryfall ID"),
-                        Quantity = csvReader.GetField<int>("Quantity"),
-                        CardFinish = csvReader.GetField<CardFinish>("Foil"),
-                        IsCommander = false
-                    });
-                } catch(Exception ex){
-                    string errorData = "";
-                    string value;
-                    for(int i=0; csvReader.TryGetField<string>(i, out value); i++) {
-                        errorData += value + ", ";
-                    }
-                    _context.ImportErrors.Add(new ImportError(){
-                        Error = ex.Message,
-                        ErrorData = errorData,
-                        ImportTaskId = task.Id,
-                    });
-                    result = false;
-                }
-            }
-
-            task.TaskState = result ? ImportTaskState.Successfull : ImportTaskState.Failed;
-            task.finishedAt = DateTime.UtcNow;
-            
-            await _context.SaveChangesAsync();
-
-            await Task.CompletedTask;
         }
     }
 }
