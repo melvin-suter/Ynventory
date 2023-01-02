08e43864b2c6cf01ed377900f3928d2e244c4843
Melvin mngmt-02 <melvin@suter.dev>
2023-01-02 18:37:09 +0100

    add missing migrations

--- src/ynventory.backend/ynventory.data/Migrations/20230102173631_import_tasks_add_dates.Designer.cs
+++ src/ynventory.backend/ynventory.data/Migrations/20230102173631_import_tasks_add_dates.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Ynventory.Data;
@@ -11,9 +12,11 @@ using Ynventory.Data;
 namespace Ynventory.Data.Migrations
 {
     [DbContext(typeof(YnventoryDbContext))]
-    partial class YnventoryDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230102173631_import_tasks_add_dates")]
+    partial class importtasksadddates
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/ynventory.backend/ynventory.data/Migrations/20230102173631_import_tasks_add_dates.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ynventory.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class importtasksadddates : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "createdAt",
+                table: "ImportTasks",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "finishedAt",
+                table: "ImportTasks",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "createdAt",
+                table: "ImportTasks");
+
+            migrationBuilder.DropColumn(
+                name: "finishedAt",
+                table: "ImportTasks");
+        }
+    }
+}
